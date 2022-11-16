37385044ee1285fa6b388feb550e2f04d068d9ce
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2022-11-17 00:54:39 +0300

    added portfolio images

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,13 @@ import web3 from '../public/web3.png'
 import web4 from '../public/web4.png'
 import web5 from '../public/web5.png'
 import web6 from '../public/web6.png'
+
+import ui1 from '../public/ui1.png'
+import ui2 from '../public/ui2.png'
+import ui3 from '../public/ui3.png'
+import ui4 from '../public/ui4.png'
+import ui5 from '../public/ui5.png'
+import ui6 from '../public/ui6.png'
 import {useState} from 'react';
 
 export default function Home() {
@@ -94,7 +101,7 @@ export default function Home() {
 
           </div>
         </section>
-        {/* 
+         
         <div>
           <h3 className="text-3xl py-1 dark:text-white">Portfolio</h3>
           <p className='text-md py-2 leading-8 text-gray-800 dark:text-teal-200'>
@@ -105,25 +112,19 @@ export default function Home() {
         </div>
         <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'> 
           <div className='basis-1/3 flex-1'>
-            <Image src={web1} className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"/>
+            <Image src={ui1} className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive"/>
           </div>
           <div className='basis-1/3 flex-1'>
-            <Image src={web2} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            <Image src={ui4} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
           </div>
           <div className='basis-1/3 flex-1'>
-            <Image src={web3} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            <Image src={ui3} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
           </div>
-          <div className='basis-1/3 flex-1'>
-            <Image src={web4} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
-          </div>
-          <div className='basis-1/3 flex-1'>
-            <Image src={web5} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
-          </div>
-          <div className='basis-1/3 flex-1'>
-            <Image src={web6} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+          <div className='basis-1/3 flex-1' height={'250px'}>
+            <Image src={ui6} className="rounded-lg object-cover" width={'100%'} height={'200%'} layout="responsive"/>
           </div>
         </div>
-        */}
+        
   
       </main>
     </div>
